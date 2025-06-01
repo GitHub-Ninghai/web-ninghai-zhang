da1e72d91e3aa0678cf22ec2e47a2f04571ed3a8
GitHub-Ninghai <1053279353@qq.com>
2025-06-01 13:50:29 +0800

    更新

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# 读取 index.html 文件
-with open("index.html", "r", encoding="utf-8") as f:
-    html_content = f.read()
-
-# 渲染 HTML
-st.title("HTML 页面展示")
-st.components.v1.html(html_content, weight=1000,height=600, scrolling=True)
